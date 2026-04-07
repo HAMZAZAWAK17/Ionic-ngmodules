7a1f0f944e03ed068504f8bb02501cf83a537010
Ez-zouek Hamza <ezzouekhamza2411@gmail.com>
2026-04-07 13:10:42 +0000

    feat: add interactive dashboard widgets page with system monitoring components

--- src/app/widgets/widgets.page.ts
+++ src/app/widgets/widgets.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { 
+  statsChartOutline, 
+  cashOutline, 
+  peopleOutline, 
+  analyticsOutline, 
+  cartOutline,
+  cellularOutline,
+  pulseOutline,
+  flashOutline,
+  batteryChargingOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-widgets',
   templateUrl: './widgets.page.html',
   styleUrls: ['./widgets.page.scss'],
+  standalone: false
 })
 export class WidgetsPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ 
+      statsChartOutline, 
+      cashOutline, 
+      peopleOutline, 
+      analyticsOutline, 
+      cartOutline,
+      cellularOutline,
+      pulseOutline,
+      flashOutline,
+      batteryChargingOutline
+    });
   }
 
+  ngOnInit() { }
 }
