4517c51a4bc0e1892257a73026e5734b78d22f1c
Ez-zouek Hamza <ezzouekhamza2411@gmail.com>
2026-04-14 07:48:38 +0000

    feat: implement game logic and icon registration for JeuxPage component

--- src/app/jeux/jeux.page.ts
+++ src/app/jeux/jeux.page.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { addIcons } from 'ionicons';
+import {
+  gameControllerOutline,
+  heart,
+  trophyOutline,
+  skullOutline,
+  pulseOutline,
+  keypadOutline,
+  checkmarkCircleOutline,
+  refreshCircleOutline,
+  refreshOutline,
+  chevronBackOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-jeux',
@@ -14,7 +27,20 @@ export class JeuxPage implements OnInit {
   jeuTermine: boolean = false;
   gagne: boolean = false;
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      gameControllerOutline,
+      heart,
+      trophyOutline,
+      skullOutline,
+      pulseOutline,
+      keypadOutline,
+      checkmarkCircleOutline,
+      refreshCircleOutline,
+      refreshOutline,
+      chevronBackOutline
+    });
+  }
 
   ngOnInit() {
     this.initialiserJeu();
@@ -24,7 +50,7 @@ export class JeuxPage implements OnInit {
     this.nombreSecret = Math.floor(Math.random() * 101);
     this.tentatives = 5;
     this.nombreSaisi = null;
-    this.message = 'Saisissez un nombre entre 0 et 100 !';
+    this.message = 'Find the number between 0 and 100 !';
     this.jeuTermine = false;
     this.gagne = false;
   }
@@ -35,18 +61,18 @@ export class JeuxPage implements OnInit {
     this.tentatives--;
 
     if (this.nombreSaisi > this.nombreSecret) {
-      this.message = 'Trop grand ! Essayez plus bas.';
+      this.message = 'Too high! Try a lower number.';
     } else if (this.nombreSaisi < this.nombreSecret) {
-      this.message = 'Trop petit ! Essayez plus haut.';
+      this.message = 'Too low! Try a higher number.';
     } else {
-      this.message = 'Bravo ! Vous avez trouvé !';
+      this.message = 'Incredible! You found it!';
       this.jeuTermine = true;
       this.gagne = true;
       return;
     }
 
     if (this.tentatives <= 0 && !this.gagne) {
-      this.message = `Perdu ! Le nombre était ${this.nombreSecret}.`;
+      this.message = `Game Over! The number was ${this.nombreSecret}.`;
       this.jeuTermine = true;
     }
     
